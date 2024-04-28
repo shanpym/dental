d7f25c80a9710799d62058a40069c8c4a8195c39
unknown <shanpym14.work@gmail.com>
2024-04-28 23:14:03 +0800

    initial design

---
+++ contact.php
@@ -0,0 +1,302 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+
+  <title>Company Name</title>
+
+  <!-- slider stylesheet -->
+  <link rel="stylesheet" type="text/css"
+    href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/assets/owl.carousel.min.css" />
+
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+  <!-- fonts style -->
+  <link href="https://fonts.googleapis.com/css?family=Dosis:400,500|Poppins:400,600,700&display=swap" rel="stylesheet">
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="css/responsive.css" rel="stylesheet" />
+</head>
+
+<body class="sub_page">
+
+  <body class="">
+    <div class="hero_area">
+      <!-- header section strats -->
+      <header class="header_section">
+        <div class="container-fluid">
+          <nav class="navbar navbar-expand-lg custom_nav-container">
+            <a class="navbar-brand" href="index.php">
+              <span>
+                DENTAL
+              </span>
+            </a>
+
+            <div class="navbar-collapse" id="">
+              <div class="d-none d-lg-flex ml-auto flex-column flex-lg-row align-items-center">
+                <ul class="navbar-nav  ">
+                <li class="nav-item">
+                  <a href="login.php" class="nav-link" href="fruit.html">
+                    <img src="images/login.png" alt="" />
+                    <span>Login</span></a>
+                </li>
+                <li class="nav-item">
+                  <a href="signup.php"  class="nav-link" href="service.html">
+                    <img src="images/signup.png" alt="" />
+                    <span>Sign Up</span>
+                  </a>
+                </li>
+                </ul>
+                <form class="form-inline my-2 mb-3 mb-lg-0  mr-5">
+                  <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit"></button>
+                </form>
+              </div>
+
+              <div class="custom_menu-btn">
+                <button onclick="openNav()">
+                  <span class="s-1">
+
+                  </span>
+                  <span class="s-2">
+
+                  </span>
+                  <span class="s-3">
+
+                  </span>
+                </button>
+              </div>
+              <div id="myNav" class="overlay">
+                <div class="overlay-content">
+                  <a href="index.php">HOME</a>
+                  <a href="team.php">TEAM</a>
+                  <a href="portfolio.php">PORTFOLIO</a>
+
+                  <a href="contact.php">CONTACT US</a>
+
+                </div>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </header>
+      <!-- end header section -->
+    </div>
+
+
+
+    <!-- contact section -->
+    <section class="contact_section layout_padding">
+      <div class="container">
+        <p>
+          Don’t Hesitate To Ask
+        </p>
+        <h2 class="">
+          FREE CONSULTATION
+        </h2>
+      </div>
+      <div class="container">
+
+        <div class="row">
+          <div class="col-md-6 ">
+            <form action="">
+              <div class="contact_form-container">
+                <div>
+                  <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Full Name</label>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Juan Dela Cruz">
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Phone No.</label>
+                    <input type="number" class="form-control" id="exampleFormControlInput1" placeholder="09X XXXX XXXX">
+                  </div>
+                    <div class="mb-3">
+                    <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                  </div>
+                  <div class="mt-5">
+                    <button type="submit">
+                      Send
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div class="col-md-6">
+            <div class="contact_img-box">
+              <img src="images/form-img.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- end contact section -->
+
+
+    <!-- info section -->
+    <section class="info_section layout_padding">
+      <div class="container info_content">
+        <div>
+          <div class="row">
+            <div class="col-md-6">
+              <div class="row">
+                <div class="col-md-6">
+                  <h5>
+                    About Us
+                  </h5>
+                  <ul>
+                    <li>
+                      <a href="">
+                        It is a long established
+                      </a>
+                    </li>
+                    <li>
+                      <a href="">
+                        fact that a reader will be
+                      </a>
+                    </li>
+                    <li>
+                      <a href="">
+                        distracted by the read
+                      </a>
+                    </li>
+                    <li>
+                      <a href="">
+                        able LoremIt is a long es
+                      </a>
+                    </li>
+                    <li>
+                      <a href="">
+                        tablished fact that a
+                      </a>
+                    </li>
+                    <li>
+                      <a href="">
+                        reader will be distracted
+                      </a>
+                    </li>
+
+                  </ul>
+                </div>
+                <div class="col-md-6">
+                  <h5>
+                    Contact Us
+                  </h5>
+                  <ul>
+                    <li>
+                      <a href="">
+                        It is a long established
+                      </a>
+                    </li>
+                    <li>
+                      <a href="">
+                        fact that a reader will be
+                      </a>
+                    </li>
+                    <li>
+                      <a href="">
+                        distracted by the read
+                      </a>
+                    </li>
+                    <li>
+                      <a href="">
+                        able LoremIt is a long es
+                      </a>
+                    </li>
+                    <li>
+                      <a href="">
+                        tablished fact that a
+                      </a>
+                    </li>
+                    <li>
+                      <a href="">
+                        reader will be distracted
+                      </a>
+                    </li>
+
+                  </ul>
+                </div>
+              </div>
+              <div class="form_container mt-5">
+                <form action="">
+                  <input type="email" placeholder="Enter Your email" />
+                  <button type="submit">
+                    Subscribe
+                  </button>
+                </form>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="info_img-box">
+                <img src="images/map.png" alt="">
+              </div>
+              <div class="d-flex justify-content-end pr-5">
+                <div class="social-box">
+                  <a href="">
+                    <img src="images/fb.png" alt="" />
+                  </a>
+
+                  <a href="">
+                    <img src="images/twitter.png" alt="" />
+                  </a>
+                  <a href="">
+                    <img src="images/linkedin1.png" alt="" />
+                  </a>
+                  <a href="">
+                    <img src="images/instagram1.png" alt="" />
+                  </a>
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+    <!-- end info section -->
+    <hr class="footer_hr">
+    <!-- footer section -->
+    <section class="container-fluid footer_section">
+      <p>
+        &copy;
+        2019 All Rights Reserved. Design by
+        <a href="https://html.design/">Free Html Templates</a>
+      </p>
+    </section>
+    <!-- footer section -->
+
+    <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+
+
+
+    <script>
+      function openNav() {
+        document.getElementById("myNav").classList.toggle("menu_width")
+        document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style")
+      }
+    </script>
+
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+
+</html>
